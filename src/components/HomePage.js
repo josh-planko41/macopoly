f9fcf1ffa0552b561a31da666672c8d5e5dafcb3
Josh Planko <jplanko@macalester.edu>
2025-10-01 11:19:49 -0500

    altering home HomePage

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePageTitle() {
+  return (
+    <h1 className="Greeting"> Welcome to Macopoly!</h1>
+  );
+}
+
+function HomePageDescription() {
+  return (
+    <p className="Description"> This is the home page for our project for <i>COMP225</i>, Fall 2025.
+    Created by Josh, Chenhao, Bavo, and Colin.</p>
+  );
+}
