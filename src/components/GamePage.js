51d29b0f20f2427209c4e018f3829a8b7e8d9a13
Josh Planko <jplanko@macalester.edu>
2025-10-01 16:29:39 -0500

    Messing with game page

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,2 +1,24 @@
 import React from 'react';
 
+export default function CreateGamePage(){
+    return (
+        <div>
+            <GPText />
+            <GPBackground />
+            {/* Replace the above two components with the actual game board and related components later */}
+        </div>
+    )
+}
+
+// Test Code to replace with board
+function GPText(){
+    return (
+        <h1 className="Greeting"> This is the Game Page.</h1>
+    )
+}
+
+function GPBackground(){
+    return (
+        <div className='GamePageBackground'></div>
+    )
+}
--- src/components/PlayButtons.js
+++ src/components/PlayButtons.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import CreateGamePage from './GamePage';
 
 export default function PlayButton() {
+    const handleClick = () => {
+        RemoveHomeText();
+        CreateGamePage();
+    }
+
     return (
-        <button className="PlayButton"> Play! </button>
+        <button id="pb" className="PlayButton" onClick={handleClick}> Play! </button>
     )
 }
 
-const Button1 = document.querySelector('.PlayButton');
+
+
+function RemoveHomeText(){
+    const greeting = document.querySelector('.Greeting');
+    const description = document.querySelector('.Description');
+    const backgroungImg = document.querySelector('.BackgroundImage');
+    const playButton = document.querySelector('.PlayButton');
+
+    if (greeting) 
+        greeting.textContent = '';
+    if (description) {
+        description.textContent = '';
+        description.style.backgroundColor = 'rgb(255, 255, 255)';
+    }
+    if (backgroungImg) 
+        backgroungImg.style.display = 'none';
+    if (playButton) 
+        playButton.remove();
+}
+
+
+
 
