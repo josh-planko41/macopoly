fbc184304824deb7dd58951ccb3e8e5e9b63f00d
Bavo27 <bvandenh@macalester.edu>
2025-10-22 11:52:49 -0400

    Dice

---
+++ src/components/Dice.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../App.css';
+import {useState} from 'react';
+
+import Img1 from '../diceImages/Number1.png';
+import Image2 from "../diceImages/Number2.png";
+import Image3 from "../diceImages/Number3.png";
+import Image4 from "../diceImages/Number4.png";
+import Image5 from "../diceImages/Number5.png";
+import Image6 from "../diceImages/Number6.png";
+
+
+export default function Roll() {
+    const diceImages = [Img1, Image2, Image3, Image4, Image5, Image6];
+
+    const [image, setNewImage] = useState(diceImages[0])
+    const [image2, setNewImage2] = useState(diceImages[1])
+
+    const rollDice = () => {
+        //Generates numbers
+        var randomNum1 = Math.floor(Math.random() * 6);
+        var randomNum2 = Math.floor(Math.random() * 6);
+        setNewImage(diceImages[randomNum1]);
+        setNewImage2(diceImages[randomNum2]);
+    }
+
+    return (
+        <div>
+            <center>
+                <h1>This is the dice roller</h1>
+                <div className="container">
+                    <img className='imgSquare' src = {image}></img>
+                <div style={{width:'5px', display:'inline-block'}}></div>
+                    <img className='imgSquare' src = {image2}></img>
+                    <button className='diceButton' onClick={rollDice}>Roll Dice</button>
+                </div>
+            </center>
+        </div>
+    )
+}
+
+function setNewImage() {
+
+}
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../App.css';
 import Board from './Board';
+import Dice from './Dice'
 
 export default function CreateGamePage(){
     return (
         <div className="GamePageBackground">
             {/* <h1 className="Greeting" id="GamePageTitle"> This is the Game Page - In Progress</h1> */}
-            <Board />
-            <button className="PlayButton" onClick={() => window.location.reload()}> Back to Home </button>
+            {/* <Board /> */}
+            <Dice />
+            {/* <button className="PlayButton" onClick={() => window.location.reload()}> Back to Home </button> */}
             {/* Replace the above two components with the actual game board and related components later */}
         </div>
     )
